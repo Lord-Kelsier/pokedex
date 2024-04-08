38500655b4bb40844076fd3af991c505c510bb58
Lord-Kelsier <econtreraslazcano@uc.cl>
2024-04-08 21:06:15 +0000

    setting port

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-});
+export default ({mode}) => {
+  const env = loadEnv(mode, process.cwd());
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      host: true,
+      port: parseInt(env.VITE_PORT),
+      watch: {
+        usePolling: true,
+        // for windows (wsl2) refresh on save
+      }
+    },
+  });
+}
