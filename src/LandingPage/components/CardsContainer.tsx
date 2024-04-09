1010f27c9c2b55b2df5dfede2065b8ce204cf88c
Eduardo Contreras <econtreraslazcano@uc.cl>
2024-04-09 05:39:30 +0000

    Adding load more button

--- src/LandingPage/components/CardsContainer.tsx
+++ src/LandingPage/components/CardsContainer.tsx
@@ -1,5 +1,6 @@
-import { PokeContainer } from './styled-components/CardStyles';
+import { PokeContainer, ButtonLoadMore } from './styled-poke-components/ContainerStyles';
 import { useFetch } from '../../services/useFetch';
+import { useEffect, useState } from 'react';
 
 import PokeCard from './PokeCard';
 
@@ -9,13 +10,21 @@ interface PokemonPreRequestInfo {
 }
 
 function CardsContainer() {
-  const { data } = useFetch('https://pokeapi.co/api/v2/pokemon?limit=15&offset=0');
+  const initialUrl = 'https://pokeapi.co/api/v2/pokemon?limit=15&offset=0';
+  const [nextUrl, setNextUrl] = useState(initialUrl);
+  const { data } = useFetch(initialUrl);
+  useEffect(() => {
+    if (data !== null) {
+      setNextUrl(data.next);
+    }
+  }, [data]);
 
   return (
     <PokeContainer>
       {data?.results.map((pokemon: PokemonPreRequestInfo, index: number) => (
         <PokeCard key={index} pokeUrl={pokemon.url} name={pokemon.name} />
       ))}
+      <ButtonLoadMore>Load More</ButtonLoadMore>
     </PokeContainer>
   );
 }
---
+++ src/shared/theme.ts
@@ -0,0 +1,3 @@
+export default {
+  blueButton: '#30a7d7',
+};
