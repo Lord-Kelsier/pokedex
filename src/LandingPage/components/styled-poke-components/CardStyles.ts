3cadd0e7104a40b3d69105a9c8e92c4dce484836
Eduardo Contreras <econtreraslazcano@uc.cl>
2024-04-10 01:46:56 +0000

    Added card animation on hover

--- src/LandingPage/components/styled-poke-components/CardStyles.ts
+++ src/LandingPage/components/styled-poke-components/CardStyles.ts
@@ -1,10 +1,19 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import getBackgroundColor from './pokeTypesColor';
 
+const jumpAnimation = keyframes`
+ 0% { transform: translateY(0); }
+ 50% { transform: translateY(-4px); }
+ 100% { transform: translateY(0); }
+`;
 export const CardBox = styled.article`
   width: 200px;
   height: 300px;
   padding: 0 5px 50px 5px;
+  &: hover {
+    animation-name: ${jumpAnimation};
+    animation-duration: 0.2s;
+  }
 `;
 
 export const PokeImage = styled.img`
