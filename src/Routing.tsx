8f7e6b2d1d7b2a25b0f3fb07da7a1797a51eb710
Eduardo Contreras <econtreraslazcano@uc.cl>
2024-04-10 05:22:33 +0000

    context created

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -1,15 +1,21 @@
+import { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import LandingPage from './LandingPage/LandingPage';
+import { PaginationContext } from './paginationContext';
 import PokemonInfo from './PokemonInfo/PokemonInfo';
+
 export default function Routing() {
+  const [pageData, setPageData] = useState([]);
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/pokemon/:pokeName" element={<PokemonInfo />} />
-        <Route path="*" element={<LandingPage />} />
-      </Routes>
+      <PaginationContext.Provider value={{ pageData, setPageData }}>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/pokemon/:pokeName" element={<PokemonInfo />} />
+          <Route path="*" element={<LandingPage />} />
+        </Routes>
+      </PaginationContext.Provider>
     </BrowserRouter>
   );
 }
